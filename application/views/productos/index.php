43cd7a440a79f1ee7c0de964dd176073588581ed
dlazarte88 <dlazarte88@gmail.com>
2016-07-16 20:33:10 -0300

    Fuentes

--- application/views/productos/index.php
+++ application/views/productos/index.php
@@ -1,13 +1,21 @@
 <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        PRODUCTOS
+        <small>advanced productos</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Productos</a></li>
+      </ol>
+    </section>
     <!-- Main content -->
     <section class="content">
       <div class="row">
         <div class="col-xs-12">
           <div class="box">
-            <div class="box-header">
-              <h3 class="box-title">Data Table With Full Features</h3>
-            </div>
             <!-- /.box-header -->
             <div class="box-body">
               <table id="productos-table" class="table table-bordered table-striped">
