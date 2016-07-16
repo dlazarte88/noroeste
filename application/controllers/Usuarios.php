6cb3be1a52ebb65ec8d550bf305425eadf5d8856
Pdsalinas <pdsalinas04@gmail.com>
2016-07-16 19:38:56 -0300

    Pagina Usuarios

---
+++ application/controllers/Usuarios.php
@@ -0,0 +1,20 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Usuarios extends CI_Controller{
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->library(array('ion_auth','form_validation'));
+	}
+
+	public function index()
+	{
+		if ($this->ion_auth->logged_in()){
+			$this->load->view('template/head');
+			$this->load->view('template/header');
+			$this->load->view('template/menu_lateral');
+			$this->load->view('usuarios/index');
+			$this->load->view('template/footer');
+		}
+	}
+}
