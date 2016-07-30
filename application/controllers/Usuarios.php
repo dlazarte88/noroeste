5da6fba1a484e7efba8df0bc98378b8b26e78090
Pdsalinas <pdsalinas04@gmail.com>
2016-07-30 19:23:42 -0300

    Usuarios Agregar Listar

--- application/controllers/Usuarios.php
+++ application/controllers/Usuarios.php
@@ -5,16 +5,53 @@ class Usuarios extends CI_Controller{
 	public function __construct(){
 		parent::__construct();
 		$this->load->library(array('ion_auth','form_validation'));
+		$this->load->model('usuario_model');
+		$this->load->model('roles_model');
 	}
 
 	public function index()
 	{
-		if ($this->ion_auth->logged_in()){
-			$this->load->view('template/head');
-			$this->load->view('template/header');
-			$this->load->view('template/menu_lateral');
-			$this->load->view('usuarios/index');
-			$this->load->view('template/footer');
+		if (!$this->ion_auth->logged_in()){
+			redirect('/');
 		}
+		$this->load->view('template/head');
+		$this->load->view('template/header');
+		$this->load->view('template/menu_lateral');
+		$this->load->view('usuarios/index');
+		$this->load->view('template/footer');
+		
+	}
+
+	public function listar()
+	{
+		if (!$this->ion_auth->logged_in()){
+			redirect('/');
+		}
+		// VALIDAR DATOS INGRESADOS
+		
+		$aData = $this->usuario_model->listar_usuario();
+		if ($aData == FALSE) {			
+			$aData['username'] = '';
+			$aData['first_name'] = '';
+			$aData['last_name'] = '';
+			$aData['email'] = '';
+			$aData['phone'] = '';
+		}
+		$aResult = array("data" => $aData);
+		echo json_encode($aResult);
+	}
+
+	public function agregar()
+	{
+		if (!$this->ion_auth->logged_in()) {
+			redirect('/');
+		}
+		$aData = $this->roles_model->listar_roles();
+		if ($aData == FALSE) {
+			$aData['id'] = '';
+			$aData['name'] = '';
+		}
+		$aResult = array("usuario_select" => $aData);
+		echo json_encode($aResult);
 	}
 }
--- js/usuarios.js
+++ js/usuarios.js
@@ -1,3 +1,55 @@
+var table_usuario;
+var url_base = "http://noroeste.localhost.net/";
+var method;
+var idUsuario;
+
 $(document).ready(function() {
-  	$("#usuarios-table").DataTable();
+  	// Datatables
+  	table_usuario = $('#usuario-table').DataTable({
+        "processing": true,
+  		"ajax": url_base + 'usuarios/listar',
+  		"columnDefs": [
+  			{"data" : "username","targets":[0]},
+  			{"data" : "first_name","targets":[1]},
+  			{"data" : "last_name","targets":[2]},
+  			{"data" : "email","targets":[3]},
+  			{"data" : "phone","targets":[4]},
+            {
+            "data" : "id",
+            "targets" :[5],
+            "orderable": false,
+            "render" : function ( data, type, full, meta ) {
+                var html = '<div class="accion-usuario">'
+                html += '<button type="button" class="btn btn-primary" data-toggle="modal" onclick="editUsuario('+data+')"><i class="glyphicon glyphicon-pencil"></i>  Editar </button>';
+                html += '<button type="button" class="btn btn-danger" data-toggle="modal" onclick="borrarUsuario('+data+')"><i class="glyphicon glyphicon-trash"></i>  Borrar </button></div>';
+                return html;
+                }
+            }
+  		]
+  	});
 });
+
+function addUsuario()
+{
+    method = 'add';
+    validarCodigo();
+    $('#add-usuario-form')[0].reset();
+    $.ajax({
+        url: url_base + 'usuarios/agregar',
+        type: "GET",
+        dataType: "JSON",
+        success: function(data)
+        {
+            $('#Grupo').html('<option value="-1" selected="selected">Seleccionar</option>');
+            for (var i = 0; i < data.usuario_select.length ; i++) {
+                $('#Grupo').append('<option value="'+data.usuario_select[i]['id']+'">'+data.usuario_select[i]['name']+'</option>');
+            };
+            $('#add-usuario-modal').modal('show');
+            $('.modal-title').text('Agregar Usuario');
+        },
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+            alert('Error get data from ajax');
+        }
+    })
+}
